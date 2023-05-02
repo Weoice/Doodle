eb42809eae7ca900203f7ecda76f0a90f3a21f8b
Weoice <84687104+Weoice@users.noreply.github.com>
2023-05-01 20:07:29 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function setup() {
+
+    Canvas = createCanvas(280, 280);
+    Canvas.center();
+    background("white");
+
+}
+
+function Clear() {
+
+background("white");
+
+}
+
+timer_counter = 0;
+timer_check = "";
+drawn_sketch = "";
+answer_holder = "";
+score = 0;
